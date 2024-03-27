526aa58810275a0c8f53679442aac4dc5f9165ec
ashx2611 <ashwiniphadke1@gmail.com>
2024-03-27 15:16:42 -0400

    changing the welcome message

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 @app.get("/")
 def root():
-   return{"message":"Hello World!!"}
+   return{"message":"Hello! Welcome to the website!!"}
 app.include_router(post.router)
 
 app.include_router(auth.router)
